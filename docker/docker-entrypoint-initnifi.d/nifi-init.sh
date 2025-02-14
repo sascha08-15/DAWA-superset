74d669a65e374eec1247802897264da30a66f618
sascha08-15 <s.a.s.c.h@gmx.de>
2025-02-14 04:16:37 +0100

    Updated nifi

--- docker/docker-entrypoint-initnifi.d/nifi-init.sh
+++ docker/docker-entrypoint-initnifi.d/nifi-init.sh
@@ -5,6 +5,7 @@ set -e
 # Path to the PostgreSQL JDBC driver
 POSTGRES_JAR_PATH="/opt/nifi/nifi-current/lib/postgresql-42.2.8.jar"
 SQLITE_JAR_PATH="/opt/nifi/nifi-current/lib/sqlite-jdbc-3.48.0.0.jar"
+JYTHON_JAR_PATH="/opt/nifi/nifi-current/lib/jython-standalone-2.7.4.jar"
 
 # Check if the file already exists
 if [ -f "$POSTGRES_JAR_PATH" ]; then
@@ -19,8 +20,17 @@ if [ -f "$SQLITE_JAR_PATH" ]; then
     echo "SQLite JDBC driver already exists at $SQLITE_JAR_PATH"
 else
     echo "Downloading SQLite JDBC driver..."
-    curl -o "$SQLITE_JAR_PATH" https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.47.0.0/sqlite-jdbc-3.47.0.0.jar
+    curl -o "$SQLITE_JAR_PATH" https://repo1.maven.org/maven2/org/xerial/sqlite-jdbc/3.48.0.0/sqlite-jdbc-3.48.0.0.jar
 fi
+
+# Check if the file already exists
+if [ -f "$JYTHON_JAR_PATH" ]; then
+    echo "SQLite Jython driver already exists at $SQLITE_JAR_PATH"
+else
+    echo "Downloading Jython lib..."
+    curl -o "$JYTHON_JAR_PATH" https://repo1.maven.org/maven2/org/python/jython-standalone/2.7.4/jython-standalone-2.7.4.jar
+fi
+
 
 
 # Call the original entrypoint script to start NiFi
